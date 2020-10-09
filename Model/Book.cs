3de1822c69eede4a50214811d45d8ed759d1d2d0
Embaby97 <45373943+embaby97@users.noreply.github.com>
2020-10-09 04:15:30 +0200

    Add files via upload

---
+++ Model/Book.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        //create id value automatically
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+
+
+    }
+}
